b5e2002489efb6d4165056c7f010400d792c1003
Marcus <marcustyphoon@gmail.com>
2021-09-23 03:48:26 -0700

    parse modifiers

--- index.js
+++ index.js
@@ -6,12 +6,34 @@ import yaml from 'js-yaml';
 const convert = async function () {
   const files = await fs.readdir('./data');
 
+  // temp
+  let count = 0;
+
   for (const fileName of files) {
+    console.log(fileName);
     const fileData = await fs.readFile(`./data/${fileName}`);
     const data = yaml.load(fileData);
 
-    console.log(JSON.stringify(data, null, 2).slice(0, 120));
+    for (const section of data.list) {
+      for (const item of section.items) {
 
+
+        // temp
+        count++;
+        if (count > 5) return;
+
+        console.log(`\n${item.id}`);
+
+        const newModifiers = {};
+
+        const parsedModifiersArray = Object.entries(JSON.parse(item.modifiers));
+        for (const [type, modifiers] of parsedModifiersArray) {
+          for (const [attribute, value] of Object.entries(modifiers)) {
+            console.log(type, attribute, value);
+          }
+        }
+      }
+    }
 
     // let resultYaml = yaml.dump(result, {
     //   forceQuotes: true,
@@ -21,6 +43,6 @@ const convert = async function () {
     // resultYaml = resultYaml.replace(/\n/g, '\n\n').replace(/\n\n    /g, '\n    ')
     // fs.writeFileSync(base.src + 'yaml/' + mode + '.yaml', resultYaml, 'utf8');
   }
-}
+};
 
 convert();
