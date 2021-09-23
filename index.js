4e4bb2e5a9e7730e018562bc705b6f40aa371680
Marcus <marcustyphoon@gmail.com>
2021-09-23 03:57:18 -0700

    log better, dump yaml

--- index.js
+++ index.js
@@ -10,36 +10,37 @@ const convert = async function () {
   let count = 0;
 
   for (const fileName of files) {
-    console.log(fileName);
+    console.log('\n', fileName);
     const fileData = await fs.readFile(`./data/${fileName}`);
     const data = yaml.load(fileData);
 
     for (const section of data.list) {
+      if (!section.items) break;
+
       for (const item of section.items) {
-
-
         // temp
         count++;
-        if (count > 5) return;
+        if (count > 60) return;
 
-        console.log(`\n${item.id}`);
+        console.log('\n  ', item.id);
 
         const newModifiers = {};
 
         const parsedModifiersArray = Object.entries(JSON.parse(item.modifiers));
         for (const [type, modifiers] of parsedModifiersArray) {
           for (const [attribute, value] of Object.entries(modifiers)) {
-            console.log(type, attribute, value);
+            console.log('  ', type, attribute, value);
           }
         }
       }
     }
 
-    // let resultYaml = yaml.dump(result, {
-    //   forceQuotes: true,
-    //   lineWidth: -1,
-    //   flowLevel: 4
-    // });
+    let resultYaml = yaml.dump(data, {
+      forceQuotes: true,
+      lineWidth: -1,
+      flowLevel: 5
+    });
+    console.log(resultYaml.slice(0, 300), '\n');
     // resultYaml = resultYaml.replace(/\n/g, '\n\n').replace(/\n\n    /g, '\n    ')
     // fs.writeFileSync(base.src + 'yaml/' + mode + '.yaml', resultYaml, 'utf8');
   }
