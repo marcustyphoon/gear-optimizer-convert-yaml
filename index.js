f67c4f554f1434b71ffedc1867055ea41962a6dd
Marcus <marcustyphoon@gmail.com>
2021-10-01 12:17:36 -0700

    Revert "go back to f@#*($# arrays"
    This reverts commit 43d43992110a9b5c804d2c1736f716a5822d4665.

--- index.js
+++ index.js
@@ -72,9 +72,9 @@ const convert = async function () {
         // console.log('\n  ', item.id);
 
         const newModifiers = {
-          damage: [],
-          attributes: [],
-          conversion: [],
+          damage: {},
+          attributes: {},
+          conversion: {},
         };
 
         const parsedModifiersArray = Object.entries(JSON.parse(item.modifiers));
@@ -87,14 +87,12 @@ const convert = async function () {
                 if (Object.keys(multiplierConvertDamage).includes(attribute)) {
                   const newAttrData = multiplierConvertDamage[attribute];
                   // damage
-                  newModifiers.damage.push({
-                    [newAttrData[0]]: [round(value * 100) + '%', newAttrData[1]],
-                  });
+                  newModifiers.damage[newAttrData[0]] = [round(value * 100) + '%', newAttrData[1]];
                 } else {
                   // percent
                   if (value < 1) value *= 100;
                   const newAttr = multiplierConvertPercent[attribute];
-                  newModifiers.attributes.push({ [newAttr]: round(value) + '%' });
+                  newModifiers.attributes[newAttr] = round(value) + '%';
                 }
 
                 break;
@@ -106,11 +104,11 @@ const convert = async function () {
                 if (points.includes(attribute)) {
                   // statpoints
                   const isConv = type === 'buff' ? 'buff' : 'converted';
-                  newModifiers.attributes.push({ [newAttr]: [value, isConv] });
+                  newModifiers.attributes[newAttr] = [value, isConv];
                 } else {
                   // percent
                   if (value < 1) value *= 100;
-                  newModifiers.attributes.push({ [newAttr]: round(value) + '%' });
+                  newModifiers.attributes[newAttr] = round(value) + '%';
                 }
                 break;
               case 'convert':
@@ -120,7 +118,7 @@ const convert = async function () {
                 for (const [source, amount] of sources) {
                   result[source] = round(amount * 100) + '%';
                 }
-                newModifiers.conversion.push({ [attribute]: result });
+                newModifiers.conversion[attribute] = result;
                 break;
               default:
                 throw type;
@@ -131,7 +129,7 @@ const convert = async function () {
         // remove empty keys
         let realNewModifiers = {};
         for (const [key, value] of Object.entries(newModifiers)) {
-          if (value.length) {
+          if (Object.keys(value).length) {
             realNewModifiers[key] = value;
           }
         }
@@ -139,14 +137,12 @@ const convert = async function () {
         // add note for bountiful oil
         if (item.id === 'bountiful-maintenance-oil') {
           realNewModifiers = {
-            'conversion': [
-              {
-                'Outgoing Healing': {
-                  'Healing Power': '0.006%',
-                  'Concentration': '0.008%',
-                },
+            'conversion': {
+              'Outgoing Healing': {
+                'Healing Power': '0.006%',
+                'Concentration': '0.008%',
               },
-            ],
+            },
           };
         }
 
@@ -169,7 +165,7 @@ const convert = async function () {
     let resultYaml = yaml.dump(data, {
       // forceQuotes: true,
       lineWidth: -1,
-      flowLevel: 7, // fileName.includes('utility') ? 8 : 7
+      flowLevel: 6, // fileName.includes('utility') ? 7 : 6
     });
 
     // add spacing
